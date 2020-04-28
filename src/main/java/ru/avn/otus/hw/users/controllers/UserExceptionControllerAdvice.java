99c9057a2a4b644c63cd073f5d72ca65e9a5ae86
avnikulin <avnikulin.niiaa@gmail.com>
2020-04-29 00:10:24 +0300

    UserNotFoundException status code resolving was migrated to UserExceptionControllerAdvice

--- src/main/java/ru/avn/otus/hw/users/controllers/UserExceptionControllerAdvice.java
+++ src/main/java/ru/avn/otus/hw/users/controllers/UserExceptionControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.avn.otus.hw.users.exceptions.UserAlreadyExistsException;
 import ru.avn.otus.hw.users.exceptions.UserIncorrectVersionException;
+import ru.avn.otus.hw.users.exceptions.UserNotFoundException;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
@@ -18,9 +19,13 @@ public class UserExceptionControllerAdvice {
         response.sendError(HttpStatus.BAD_REQUEST.value());
     }
 
-
     @ExceptionHandler(value = {UserAlreadyExistsException.class, UserIncorrectVersionException.class})
     void handleConflicts(HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.CONFLICT.value());
+    }
+
+    @ExceptionHandler(value = {UserNotFoundException.class})
+    void handleNotFound(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.NOT_FOUND.value());
     }
 }
--- src/main/java/ru/avn/otus/hw/users/exceptions/UserNotFoundException.java
+++ src/main/java/ru/avn/otus/hw/users/exceptions/UserNotFoundException.java
@@ -1,9 +1,5 @@
 package ru.avn.otus.hw.users.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends ServiceException {
 
     static final long serialVersionUID = -8925672226324077815L;
@@ -11,4 +7,5 @@ public class UserNotFoundException extends ServiceException {
     public UserNotFoundException(long userId) {
         super(String.format("A user is not found with that id '%d'", userId));
     }
+
 }
